5bbe8e2d22d655b1d0172de0d718928bfba27bb3
phillipe henriques <phillipe_ch@hotmail.com>
2022-07-26 00:15:29 -0300

    while / do while /for

---
+++ pizza.js
@@ -0,0 +1,17 @@
+
+var pizza = 8 
+while(pizza >0) {
+    console.log('- 1 pedaço')
+    pizza--
+}
+
+var bolo = 1
+do {
+    console.log(`comeu ${bolo} pedaço de bolo`)
+    bolo++
+} while (bolo <=5)
+
+for(var torta = 1;torta <=4;torta++) {
+    console.log('Eu comi mais um pedaço')
+}   console.log('Eu comi 4 pedaços de torta!')
+
